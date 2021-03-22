26cc628f6f42923aafe48206434b88ab98171d9b
Diego Navea <d.navea89@gmail.com>
2021-03-22 22:03:22 +1100

    TwoFactor Authentication

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,9 +9,18 @@ const mix = require('laravel-mix');
  | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
- */
+ 
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+*/
+    let theme = process.env.npm_config_theme;
+
+    if(theme) {
+       require(`${__dirname}/themes/${theme}/webpack.mix.js`);
+    } else {
+        // default theme to compile if theme is not specified
+      require(`${__dirname}/themes/theme-name/webpack.mix.js`);
+    }
